561ea275673a09e36dcdf6334e11789dd4163bf0
osman <osman@textrecruit.com>
2018-04-26 15:19:19 -0700

    Just in case, if no user could be found, we should return null to support previous behavior.

--- src/main/java/com/untzuntz/ustackserverapi/auth/APIClientKeyUserAuth.java
+++ src/main/java/com/untzuntz/ustackserverapi/auth/APIClientKeyUserAuth.java
@@ -104,7 +104,8 @@ public class APIClientKeyUserAuth implements AuthenticationInt<UserAccount> {
 			userName = params.get(ParamNames.username);
 			UserAccount user = UserAccount.getUser(userName);
 			if (user == null) {
-				throw new APIAuthenticationException("API does not have access to given username");
+				// If there's no user, we will support the previous behavior and return null.
+				return null;
 			}
 
 			// Make sure a common customer ID exists between API Client and User.
